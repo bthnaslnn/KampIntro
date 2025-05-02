681ff3e90bbd768206230980b2e5d397879d2f85
Cİvohan <Cvohan@DESKTOP-L9B4E69>
2025-05-03 01:39:39 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string adi = "Batuhan";
+        int yas = 25;
+
+        Kurs kurs1 = new Kurs();
+        kurs1.KursAdi = "C#";
+        kurs1.Egitmen = "Batuhan Aslan";
+        kurs1.IzlenmeOrani = 68;
+
+        Kurs kurs2 = new Kurs();
+        kurs2.KursAdi = "Java";
+        kurs2.Egitmen = "Fethi Aslan";
+        kurs2.IzlenmeOrani = 64;
+
+        Kurs kurs3 = new Kurs();
+        kurs3.KursAdi = "Python";
+        kurs3.Egitmen = "Gülbahar Aslan";
+        kurs3.IzlenmeOrani = 80;
+
+        Kurs kurs4 = new Kurs();
+        kurs4.KursAdi = "C++";
+        kurs4.Egitmen = "Hüma - Ayça Nil Aslan";
+        kurs4.IzlenmeOrani = 100;
+
+
+
+        //Console.WriteLine(kurs1.KursAdi + " : " + kurs1.Egitmen);
+
+        Kurs[] kurslar = new Kurs[] {kurs1,kurs2,kurs3,kurs4}; // Kurs tipinde kurslar dizisini oluşturduğumuzda dinamik yapı elde etmiş oluyoruz.
+
+        foreach (Kurs kurs in kurslar)
+        {
+            Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen);
+        }
+
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Kurs
+{
+    public string KursAdi { get; set; }
+    public string Egitmen { get; set; }
+    public int IzlenmeOrani { get; set; }
+}
---
+++ Metotlar/Urun.cs
@@ -0,0 +1,20 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class Urun   //ÜRÜNÜ ANLATAN BİR VERİ TİPİ
+    {
+
+        //Property -- Özellik manası.
+        public int ID { get; set; }   // Bir datayı diğerlerinden ayırt edebilmemiz için eşsiz değer.
+        public string Adi { get; set; }
+        public double Fiyati { get; set; }
+        public string Aciklama { get; set; }
+        public int StokAdedi { get; set; }
+
+    }
+}
